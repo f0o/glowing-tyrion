e895e35439ef10ff6eb49d55618c9a5bdcdcd953
f0o <f0o@devilcode.org>
2014-12-03 14:31:34 +0000

    Added: 	Contacts-gathering and logging

--- includes/alerts.inc.php
+++ includes/alerts.inc.php
@@ -45,7 +45,7 @@ function gensql($rule) {
 		}
 		$i++;
 	}
-	$sql = "SELECT ".$tables[0].".device_id FROM ".implode(",",$tables)." WHERE (".$join."".$tables[0].".device_id = ?) && (".str_replace("%","",$rule).")";
+	$sql = "SELECT * FROM ".implode(",",$tables)." WHERE (".$join."".$tables[0].".device_id = ?) && (".str_replace("%","",$rule).")";
 	return $sql;
 }
 
@@ -56,31 +56,83 @@ function gensql($rule) {
  * @return void
  */
 function runrules($device) {
+	global $debug;
 	foreach( dbFetchRows("SELECT * FROM alert_rules WHERE alert_rules.disabled = 0 && ( alert_rules.device_id = -1 || alert_rules.device_id = ? ) ORDER BY device_id,id",array($device)) as $rule ) {
 		echo " #".$rule['id'].":";
-		$chk = dbFetchRows("SELECT state FROM alerts WHERE rule_id = ? && device_id = ? ORDER BY id DESC LIMIT 1", array($rule['id'], $device));
-		if( $chk[0]['state'] === "2" ) {
+		$chk = dbFetchRow("SELECT state,message FROM alerts WHERE rule_id = ? && device_id = ? ORDER BY id DESC LIMIT 1", array($rule['id'], $device));
+		if( $chk['state'] === "2" ) {
 			echo " SKIP  ";
 		}
 		$sql = gensql($rule['rule']);
 		$qry = dbFetchRows($sql,array($device));
 		if( sizeof($qry) > 0 ) {
-			if( $chk[0]['state'] === "1" ) {
+			if( $chk['state'] === "1" ) {
 				echo " NOCHG ";
 			} else {
-				if( dbInsert(array('state' => 1, 'device_id' => $device, 'rule_id' => $rule['id']),'alerts') ){
+				if( dbInsert(array('state' => 1, 'device_id' => $device, 'rule_id' => $rule['id'], 'message' => json_encode(getContacts($qry))),'alerts') ) {
 					echo " ALERT ";
 				}
 			}
 		} else {
-			if( $chk[0]['state'] === "0" ) {
+			if( $chk['state'] === "0" ) {
 				echo " NOCHG ";
 			} else {
-				if( dbInsert(array('state' => 0, 'device_id' => $device, 'rule_id' => $rule['id']),'alerts') ){
-					echo " OK    ";
+				if( dbInsert(array('state' => 0, 'device_id' => $device, 'rule_id' => $rule['id'], 'message' => $chk['message']),'alerts') ){
+					echo " OK ";
 				}
 			}
 		}
 	}
+}
+
+/**
+ * Find contacts for alert
+ * @param array $results Rule-Result
+ * @return array
+ */
+function getContacts($results) {
+	global $config;
+	if( sizeof($results) == 0 ) {
+		return array();
+	}
+	$contacts = array();
+	$uids = array();
+	foreach( $results as $result ) {
+		$tmpa = array();
+		$tmp  = NULL;
+		if( is_numeric($result["port_id"]) ) {
+			$tmpa = dbFetchRows("SELECT user_id FROM ports_perms WHERE access_level >= 0 AND port_id = ?",array($result["port_id"]));
+			foreach( $tmpa as $tmp ) {
+				$uids[$tmp['user_id']] = $tmp['user_id'];
+			}
+		}
+		if( is_numeric($result["device_id"]) ) {
+			$tmpa = dbFetchRow("SELECT sysContact FROM devices WHERE device_id = ?",array($result["device_id"]));
+			$contacts[$tmpa["sysContact"]] = "NOC";
+			$tmpa = dbFetchRows("SELECT user_id FROM devices_perms WHERE access_level >= 0 AND device_id = ?", array($result["device_id"]));
+			foreach( $tmpa as $tmp ) {
+				$uids[$tmp['user_id']] = $tmp['user_id'];
+			}
+		}
+	}
+	if( $config["alert"]["globals"] ) {
+		$tmpa = dbFetchRows("SELECT realname,email FROM users WHERE level >= 5 AND level < 10");
+		foreach( $tmpa as $tmp ) {
+			$contacts[$tmp['email']] = $tmp['realname'];
+		}
+	}
+	if( $config["alert"]["admins"] ) {
+		$tmpa = dbFetchRows("SELECT realname,email FROM users WHERE level = 10");
+		foreach( $tmpa as $tmp ) {
+			$contacts[$tmp['email']] = $tmp['realname'];
+		}
+	}
+	if( is_array($uids) ) {
+		foreach( $uids as $uid ) {
+			$tmp = dbFetchRow("SELECT realname,email FROM users WHERE user_id = ?", array($uid));
+			$contacts[$tmp['email']] = $tmp['realname'];
+		}
+	}
+	return $contacts;
 }
 ?>
