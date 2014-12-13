0a4b894f881ffb098b9dc541eba494173fc579f0
f0o <f0o@devilcode.org>
2014-12-13 10:59:52 +0000

    Fixed: 	Copyright notice

--- alerts.php
+++ alerts.php
@@ -1,6 +1,6 @@
 #!/usr/bin/env php
 <?php
-/* Copyright (C) 2014  <f0o@devilcode.org>
+/* Copyright (C) 2014 Daniel Preussker <f0o@devilcode.org>
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
